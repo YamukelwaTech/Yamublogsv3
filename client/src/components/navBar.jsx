fb4476d225dcd7d917466fc1f1d2b1bff50b9a4d
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-07-22 15:25:02 +0200

    Improved navbar to use css only

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaBars, FaTimes } from 'react-icons/fa';
+import { FaBars, FaTimes } from "react-icons/fa";
 import blog from "assets";
 
 const navLinks = [
@@ -9,41 +9,39 @@ const navLinks = [
   { title: "PostMe", url: "/newpost" },
 ];
 
-const Navbar = () => {
-  const [isMobile, /* setIsMobile */] = useState(false);
+const NavBar = () => {
   const [showModal, setShowModal] = useState(false);
-  const isSignedIn = true;
 
   const handleToggleModal = () => {
-    setShowModal(!showModal);
+    setShowModal((prevShowModal) => !prevShowModal);
   };
 
   return (
-    <nav className={`w-full bg-cColor1 ${isMobile ? "py-4 px-4" : "py-7 px-8 md:px-44"} sticky top-0 z-50`}>
+    <nav className="w-full bg-customColor1 py-4 px-4 md:py-7 md:px-8 lg:px-44 sticky top-0 z-50">
       <div className="flex justify-between items-center">
         <div className="text-customColor2 font-bold text-xl flex items-center">
           YamukelwaBlogs
           <img src={blog} alt="Blog Icon" className="ml-2 h-6 w-6" />
         </div>
-        {isMobile ? (
-          <div className="flex items-center gap-4 text-customColor2 cursor-pointer">
-            <FaBars
-              onClick={handleToggleModal}
-              className="text-customColor2 cursor-pointer"
-              aria-label="Open menu"
-            />
-          </div>
-        ) : (
-          <ul className="flex gap-4 md:gap-8 items-center justify-center text-center cursor-pointer">
-            {navLinks.map((link, index) => (
-              <li key={index} className={`text-customColor2 font-bold text-l hover:underline ${(!isSignedIn && index > 0) ? 'pointer-events-none opacity-50' : ''}`}>
-                <Link to={link.url}>{link.title}</Link>
-              </li>
-            ))}
-          </ul>
-        )}
+        <div className="flex items-center gap-4 text-customColor2 md:hidden">
+          <FaBars
+            onClick={handleToggleModal}
+            className="text-customColor2 cursor-pointer"
+            aria-label="Open menu"
+          />
+        </div>
+        <ul className="hidden md:flex gap-4 md:gap-8 items-center justify-center text-center cursor-pointer">
+          {navLinks.map((link, index) => (
+            <li
+              key={index}
+              className="text-customColor2 font-bold text-l hover:underline"
+            >
+              <Link to={link.url}>{link.title}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
-      {showModal && isMobile && (
+      {showModal && (
         <div className="fixed inset-0 z-50 flex justify-center items-center">
           <div className="absolute inset-0 bg-customColor1 opacity-90" />
           <FaTimes
@@ -57,7 +55,7 @@ const Navbar = () => {
               {navLinks.map((link, index) => (
                 <span
                   key={index}
-                  className={`text-customColor2 font-bold text-lg cursor-pointer hover:underline ${(!isSignedIn && index > 0) ? 'pointer-events-none opacity-50' : ''}`}
+                  className="text-customColor2 font-bold text-lg cursor-pointer hover:underline"
                   onClick={handleToggleModal}
                 >
                   <Link to={link.url}>{link.title}</Link>
@@ -71,4 +69,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default NavBar;
